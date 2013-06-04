cc466180271996b3fde190c0ba8721ae71712a63
Jason Zopf <jasonzopf@gmail.com>
2013-06-04 18:23:26 -0400

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Restaurant::Application.routes.draw do
   resources :menu_items do
+    # look at how the comments are a nested resource
+    # this is because each comment belongs to a menu_item
     resources :comments
   end
   # The priority is based upon order of creation:
@@ -51,7 +53,7 @@ Restaurant::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'menu_items#index'
 
   # See how all your routes lay out with "rake routes"
 
